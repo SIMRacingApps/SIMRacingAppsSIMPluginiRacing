b79541f8c0acb18473e9f4b740d31e8975334eb1
jfgilliam@yahoo.com <jfgilliam@yahoo.com>
2022-03-08 11:38:53 -0500

    Updates for the 2022 Next Gen Cars and fixes to the brake bias

--- src/com/SIMRacingApps/SIMPlugins/iRacing/Gauges/BrakeBias.java
+++ src/com/SIMRacingApps/SIMPlugins/iRacing/Gauges/BrakeBias.java
@@ -3,6 +3,7 @@
  */
 package com.SIMRacingApps.SIMPlugins.iRacing.Gauges;
 
+import com.SIMRacingApps.Car;
 import com.SIMRacingApps.Data;
 import com.SIMRacingApps.Server;
 import com.SIMRacingApps.Track;
@@ -24,6 +25,7 @@ public class BrakeBias extends iRacingGauge {
 
     private double m_brakeBiasOffset = 0.0;
     private String m_sBrakeBiasSetup = "";
+    private double m_brakeBias = 9999.0;
     
     public BrakeBias(String type, iRacingCar car, Track track, IODriver IODriver) {
         super(type, car, track, IODriver, "dcBrakeBias","%",null,null);
@@ -32,18 +34,46 @@ public class BrakeBias extends iRacingGauge {
     @Override public Data getValueCurrent(String UOM) {
         Data d = super.getValueCurrent(UOM);
         
-        if (Server.getArg("brake-bias-correction", true)) {
+        /*
+         * brake-bias-correction              TRUE  TRUE   FALSE  FALSE
+         * brake-bias-correction-{CARNAME}    TRUE  FALSE  TRUE   FALSE
+         * 
+         * -------------------------------    TRUE  FALSE  TRUE   FALSE
+         */
+        boolean brake_bias_correction = Server.getArg("brake-bias-correction", true);
+        if (brake_bias_correction)
+            brake_bias_correction = Server.getArg("brake-bias-correction-" + this.m_car.getName().getString().replace(' ', '_'),true);
+        else
+            brake_bias_correction = Server.getArg("brake-bias-correction-" + this.m_car.getName().getString().replace(' ', '_'),false);
+
+        if (brake_bias_correction) {
             String sBrakeBiasSetup = m_IODriver.getSessionInfo().getString("CarSetup","Chassis","Front","FrontBrakeBias");
+            double brakeBias       = d.getDouble();
             
-            //if the setup value changes, recalculate offset
-            if (sBrakeBiasSetup.length() > 0 && !m_sBrakeBiasSetup.equals(sBrakeBiasSetup)) {
-                m_sBrakeBiasSetup = sBrakeBiasSetup; //save setup value for detecting changes
-                sBrakeBiasSetup = sBrakeBiasSetup.substring(0, sBrakeBiasSetup.length()-1);  //strip off the % sign
+            //recalculate offset if something has changed
+            if (!sBrakeBiasSetup.isEmpty()
+            &&  !sBrakeBiasSetup.equals("0%")
+            &&  (!sBrakeBiasSetup.equals(m_sBrakeBiasSetup)  //previous setup value is different
+//                 ||
+//                 Math.abs(m_brakeBias - brakeBias) > Server.getArg("brake-bias-max-adjustment-range", 12.0) //previous bias reading is greater than the maximum range
+                )
+            &&  //and you just entered the session
+                (this.m_car.getStatus().getString().equals(Car.Status.INPITSTALL)
+                 ||
+                 this.m_car.getStatus().getString().equals(Car.Status.LEAVINGPITS)
+                 ||
+                 this.m_car.getStatus().getString().equals(Car.Status.ONPITROAD)
+                 ||
+                 this.m_car.getStatus().getString().equals(Car.Status.ONTRACK)
+                )
+            ) {
                 try {
-                    double brakeBiasSetup = Double.parseDouble(sBrakeBiasSetup);
-                    double brakeBias      = d.getDouble();
-                    
+                    double brakeBiasSetup = Double.parseDouble(sBrakeBiasSetup.substring(0, sBrakeBiasSetup.length()-1)); //strip off percent sign
+
                     m_brakeBiasOffset = brakeBiasSetup - brakeBias;
+                    Server.logger().fine(String.format("Calculating Brake Bias Offset: Setup Prev=%s Curr=%s, dcBrakeBias Prev=%f Curr=%f, Offset=%f", m_sBrakeBiasSetup,sBrakeBiasSetup,m_brakeBias,brakeBias,m_brakeBiasOffset));
+                    m_sBrakeBiasSetup = sBrakeBiasSetup; //save setup value for detecting changes
+                    m_brakeBias       = brakeBias;
                 }
                 catch (NumberFormatException e) {}
             }
